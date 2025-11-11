850095c06a5481798a7683704ead4bed24faa37c
AamirKT <aamirt@hotmail.co.uk>
2025-11-11 01:21:38 +0000

    add terraform code

---
+++ lint/modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  description = "AMI for Ubuntu EC2 instance"
+  type = string
+  default = "ami-046c2381f11878233"
+}
+
+variable "instance_type" {
+    description = "EC2 instance type"
+    type = string
+    default = "t2.micro"
+}
