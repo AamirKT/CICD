db86b08c53955ceac9c79a72b0a50025ec3f2473
AamirKT <aamirt@hotmail.co.uk>
2025-11-11 01:29:35 +0000

    format tf files

--- lint/modules/ec2/variables.tf
+++ lint/modules/ec2/variables.tf
@@ -1,11 +1,11 @@
 variable "ami" {
   description = "AMI for Ubuntu EC2 instance"
-  type = string
-  default = "ami-046c2381f11878233"
+  type        = string
+  default     = "ami-046c2381f11878233"
 }
 
 variable "instance_type" {
-    description = "EC2 instance type"
-    type = string
-    default = "t2.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
